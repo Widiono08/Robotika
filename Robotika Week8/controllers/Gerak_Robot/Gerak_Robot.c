06fc01e135be2ecfdeb53c6bd90ef48e4d6000f0
widiono <151390095+Widiono08@users.noreply.github.com>
2024-11-16 03:42:46 +0700

    https://youtu.be/6EhKvb98Z68
    https://youtu.be/6EhKvb98Z68

---
+++ Robotika Week8/controllers/Gerak_Robot/Gerak_Robot.c
@@ -0,0 +1,67 @@
+/*
+ * File:          Gerak_Robot.c
+ * Date:
+ * Description:
+ * Author:
+ * Modifications:
+ */
+
+/*
+ * You may need to add include files like <webots/distance_sensor.h> or
+ * <webots/motor.h>, etc.
+ */
+#include <webots/robot.h>
+#include <webots/motor.h>
+/*
+ * You may want to add macros here.
+ */
+#define TIME_STEP 64
+
+/*
+ * This is the main program.
+ * The arguments of the main function can be specified by the
+ * "controllerArgs" field of the Robot node
+ */
+int main(int argc, char **argv) {
+  /* necessary to initialize webots stuff */
+  wb_robot_init();
+
+  /*
+   * You should declare here WbDeviceTag variables for storing
+   * robot devices like this:
+   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
+   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
+   */
+   WbDeviceTag motor_kanan = wb_robot_get_device("right wheel motor");
+   WbDeviceTag motor_kiri = wb_robot_get_device("left wheel motor");
+
+  /* main loop
+   * Perform simulation steps of TIME_STEP milliseconds
+   * and leave the loop when the simulation is over
+   */
+   wb_motor_set_position(motor_kanan, INFINITY);
+   wb_motor_set_position(motor_kiri , INFINITY);
+  while (wb_robot_step(TIME_STEP) != -1) {
+    /*
+     * Read the sensors :
+     * Enter here functions to read sensor data, like:
+     *  double val = wb_distance_sensor_get_value(my_sensor);
+     */
+
+    /* Process sensor data here */
+
+    /*
+     * Enter here functions to send actuator commands, like:
+     * wb_motor_set_position(my_actuator, 10.0);
+     */
+     wb_motor_set_velocity(motor_kanan, 5.0);
+     wb_motor_set_velocity(motor_kiri, -5.0);
+  };
+
+  /* Enter your cleanup code here */
+
+  /* This is necessary to cleanup webots resources */
+  wb_robot_cleanup();
+
+  return 0;
+}
---
+++ Robotika Week8/controllers/Gerak_Robot/build/release/Gerak_Robot.d
@@ -0,0 +1,5 @@
+build/release/Gerak_Robot.o: Gerak_Robot.c \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
